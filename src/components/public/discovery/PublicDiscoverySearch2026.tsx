467aef7cbf5372a852b4ea0029e7541f7a7d3d55
hamed665 <hamedarezoo900@gmail.com>
2026-06-19 20:27:31 +0400

    Remove discovery search card badge

--- src/components/public/discovery/PublicDiscoverySearch2026.tsx
+++ src/components/public/discovery/PublicDiscoverySearch2026.tsx
@@ -89,7 +89,6 @@ export function PublicDiscoverySearch2026({ config }: Props) {
         <input type="hidden" name="contentType" value={copy.contentType} />
         <div className="dm2026-home-search__main">
           <div className="dm2026-home-search__header dm2026-public-discovery-search__header">
-            <span className="dm2026-badge dm2026-public-discovery-search__eyebrow">{copy.badge}</span>
             <div>
               <h2 id={titleId}>{copy.title}</h2>
               <p>{copy.description}</p>
