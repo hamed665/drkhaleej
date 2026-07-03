f2c90e4501df993f7e9b186786524f521702430b
hamed665 <hamedarezoo900@gmail.com>
2026-07-03 06:13:52 +0400

    Add public center location helper

---
+++ src/lib/catalog/public-center-location-extra.ts
@@ -0,0 +1,5 @@
+import type { PublicCenterDetail } from './public-types';
+
+export async function loadPublicCenterLocationExtra(center: PublicCenterDetail): Promise<PublicCenterDetail> {
+  return center;
+}
