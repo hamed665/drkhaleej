75277ec03c8fdc5676a1d3c51d321036ab0c9e95
hamed665 <hamedarezoo900@gmail.com>
2026-07-06 18:04:07 +0400

    Fix admin gate batch id check (#787)

--- src/server/admin/import-sitemap-eligibility.ts
+++ src/server/admin/import-sitemap-eligibility.ts
@@ -73,7 +73,7 @@ function createImportSitemapEligibilityClient(): ImportSitemapEligibilityClient 
 }
 
 function isUuid(value: string): boolean {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(value);
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(value);
 }
 
 function isRecord(value: unknown): value is JsonRecord {
